988cc2c6813e6d1e98fa0d24446baffba79f5c25
Vaibhav Agarwal <42164841+agvaibhav@users.noreply.github.com>
2018-08-07 18:05:21 +0530

    Add files via upload

--- lists.py
+++ lists.py
@@ -1,25 +1,26 @@
-if __name__ == '__main__':
-    N = int(input())
-    l=[]
-    for k in range(N):
-        cmd= input().split()
-        if len(cmd)==3:
-            i=int(cmd[1])
-            e=int(cmd[2])
-            if cmd[0]=="insert":
-                l.insert(i,e)
-        elif len(cmd)==2:
-            e=int(cmd[1])
-            if cmd[0]=='remove':
-                l.remove(e)
-            elif cmd[0]=='append':
-                l.append(e)
-        elif len(cmd)==1:
-            if cmd[0]=='sort':
-                l.sort()
-            elif cmd[0]=='pop':
-                l.pop()
-            elif cmd[0]=='reverse':
-                l.reverse()
-            elif cmd[0]=='print':
-                print(l)
+n=int(input("Write the no of commands:"))
+l=[]
+for k in range(n):
+    user_input = input("write the command:").split()
+    
+    if len(user_input)==3:
+        i=int(user_input[1])
+        e=int(user_input[2])
+        if user_input[0]=="insert":
+            l.insert(i,e)
+    elif len(user_input)==2:
+        e=int(user_input[1])
+        if user_input[0]=="append":
+            l.append(e)
+        elif user_input[0]=="remove":
+            l.remove(e)
+    elif len(user_input)==1:
+        if user_input[0]=="sort":
+            l.sort()
+        elif user_input[0]=="pop":
+            l.pop()
+        elif user_input[0]=="reverse":
+            l.reverse()
+        elif user_input[0]=="print":
+            print(l)
+        
