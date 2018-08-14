0194fc66f9142e9106a3a63865c65968b5ef6c03
Vaibhav Agarwal <42164841+agvaibhav@users.noreply.github.com>
2018-08-14 15:03:33 +0530

    Update capitalize.py

--- capitalize.py
+++ capitalize.py
@@ -1,9 +1,2 @@
 def solve(s):
-    l=[]
-    user_input=s.split()
-    for i in user_input:
-        cap=i[0].upper()
-        name=cap+i[1:]
-        l.append(name)
-    user_output=' '.join(l)
-    return user_output
+   print ' '.join(word.capitalize() for word in raw_input().split(' '))
